30485177c64b4faeccde0ba88c762e88fd12608e
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-04 10:08:05 -0300

    Create card

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -33,20 +33,37 @@ const Welcome = () => {
             <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
               Reliability
             </div>
-            <div className={companyCommonStyles}>
-              Security
-            </div>
+            <div className={companyCommonStyles}>Security</div>
             <div className={`rounded-tr-2xl ${companyCommonStyles}`}>
               Ethereum
             </div>
             <div className={`rounded-bl-2xl ${companyCommonStyles}`}>
               Web 3.0
             </div>
-            <div className={companyCommonStyles}>
-              Low Fees
-            </div>
+            <div className={companyCommonStyles}>Low Fees</div>
             <div className={`rounded-br-2xl ${companyCommonStyles}`}>
               Blockchain
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
+            <div className="flex justify-between flex-col w-full h-full">
+              <div className="flex justify-between items-start">
+                <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                  <SiEthereum fontSize={21} color="#fff" />
+                </div>
+                <BsInfoCircle fontSize={17} color="#fff" />
+              </div>
+              <div>
+                <p className="text-white font-light text-sm">
+                  {/* {shortenAddress(currentAccount)} */}
+                </p>
+                <p className="text-white font-semibold text-lg mt-1">
+                  Ethereum
+                </p>
+              </div>
             </div>
           </div>
         </div>
