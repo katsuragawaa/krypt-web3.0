c775b85231d1d755c549e96a5e98d6352eec443d
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-04 10:44:43 -0300

    Small ajusts

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,7 +1,8 @@
 import { AiFillPlayCircle } from 'react-icons/ai';
 import { SiEthereum } from 'react-icons/si';
 import { BsInfoCircle } from 'react-icons/bs';
-import { Loader } from './';
+
+import { Loader } from '.';
 
 const companyCommonStyles =
   'min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white';
@@ -20,36 +21,41 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 const Welcome = () => {
   const connectWallet = () => {};
   const handleSubmit = () => {};
+  const handleChange = () => {};
 
   return (
     <div className="flex w-full justify-center items-center">
-      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start flex-col md:mr-10">
+      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
-            Send Cryto <br /> across the world
+            Send Crypto <br /> across the world
           </h1>
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
             Explore the crypto world. Buy and sell cryptocurrencies easily on
             Krypto.
           </p>
-          <button
-            type="button"
-            onClick={connectWallet}
-            className="flex flex-row justify-center items-center my-5 bg-[#2952e3]
-            p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
-          >
-            <p className="text-white text-base font-semibold">Connect Wallet</p>
-          </button>
+          {true && (
+            <button
+              type="button"
+              onClick={connectWallet}
+              className="flex flex-row justify-center items-center my-5 bg-[#2952e3] py-3 px-4 rounded-full cursor-pointer hover:bg-[#2546bd]"
+            >
+              <AiFillPlayCircle className="text-white mr-2" />
+              <p className="text-white text-base font-semibold">
+                Connect Wallet
+              </p>
+            </button>
+          )}
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
               Reliability
             </div>
             <div className={companyCommonStyles}>Security</div>
-            <div className={`rounded-tr-2xl ${companyCommonStyles}`}>
+            <div className={`sm:rounded-tr-2xl ${companyCommonStyles}`}>
               Ethereum
             </div>
-            <div className={`rounded-bl-2xl ${companyCommonStyles}`}>
+            <div className={`sm:rounded-bl-2xl ${companyCommonStyles}`}>
               Web 3.0
             </div>
             <div className={companyCommonStyles}>Low Fees</div>
@@ -60,7 +66,7 @@ const Welcome = () => {
         </div>
 
         <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
-          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
+          <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism">
             <div className="flex justify-between flex-col w-full h-full">
               <div className="flex justify-between items-start">
                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
@@ -77,34 +83,32 @@ const Welcome = () => {
             </div>
           </div>
 
-          <div
-            className="p-5 sm:w-96 w-full flex flex-col justify-start
-          items-center blue-glassmorphism"
-          >
+          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
             <Input
-              placeholder="Address to"
+              placeholder="Address To"
               name="addressTo"
               type="text"
-              handlerChange={() => {}}
+              handleChange={handleChange}
             />
             <Input
               placeholder="Amount (ETH)"
               name="amount"
               type="number"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
             <Input
               placeholder="Keyword (Gif)"
               name="keyword"
               type="text"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
             <Input
               placeholder="Enter Message"
               name="message"
               type="text"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
+
             <div className="h-[1px] w-full bg-gray-400 my-2" />
 
             {false ? (
