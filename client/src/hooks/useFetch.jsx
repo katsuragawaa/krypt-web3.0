d547650518657a977c9afb0267ccc1b17040d1e2
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-04 22:30:03 -0300

    Fix fetch

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
-const API_KEY = import.meta.env.VITE_GIPHY_API;
+const APIKEY = import.meta.env.VITE_GIPHY_API;
 
 const useFetch = ({ keyword }) => {
   const [gifUrl, setGifUrl] = useState('');
@@ -8,16 +8,16 @@ const useFetch = ({ keyword }) => {
   const fetchGif = async () => {
     try {
       const response = await fetch(
-        `https://api/giphy.com/va/gifs/search?api_key${API_KEY}&q=${keyword
+        `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${keyword
           .split(' ')
           .join('')}&limit=1`
       );
       const { data } = await response.json();
 
-      setGifUrl(data[0]?.images?.downsized_medium?.url);
+      setGifUrl(data[0]?.images?.downsized_medium.url);
     } catch (error) {
       setGifUrl(
-        'https://i.pinimg.com/originals/73/d3/a1/73d3a14d212314ab1f7268b71d639c15.gif'
+        'https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284'
       );
     }
   };
