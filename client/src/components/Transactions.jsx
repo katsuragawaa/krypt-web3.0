edbe93abdb13df78d2766f96d8205bb320330ccc
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-04 22:32:27 -0300

    Add local stored transactions

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 
 import { TransactionContext } from '../context/TransactionContext';
 
-import useFetch from "../hooks/useFetch";
+import useFetch from '../hooks/useFetch';
 import dummyData from '../utils/dummyData';
 import { shortenAddress } from '../utils/shortenAddress';
 
@@ -28,7 +28,7 @@ const TransactionCard = ({
       flex-col p-3 rounded-md hover:shadow-2xl"
     >
       <div className="flex flex-col items-center w-full mt-3">
-        <div className="w-full mb-6 p-2">
+        <div className="display-flex justify-start w-full mb-6 p-2">
           <a
             href={`https://ropsten.etherscan.io/address/${addressFrom}`}
             target="_blank"
@@ -43,15 +43,15 @@ const TransactionCard = ({
             target="_blank"
             rel="noreferrer"
           >
-            <p className="text-white text-base mt-1">
+            <p className="text-white text-base">
               To: {shortenAddress(addressTo)}
             </p>
           </a>
-          <p className="text-white text-base mt-1">Amount: {amount} ETH</p>
+          <p className="text-white text-base">Amount: {amount} ETH</p>
           {message && (
             <>
               <br />
-              <p className="text-white text-base mt-1">Message: {message}</p>
+              <p className="text-white text-base">Message: {message}</p>
             </>
           )}
         </div>
@@ -61,7 +61,7 @@ const TransactionCard = ({
           className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"
         />
         <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
-          <p className="text-white font-bold text-sm">{timestamp}</p>
+          <p className="text-[#37c7da] font-bold">{timestamp}</p>
         </div>
       </div>
     </div>
@@ -69,7 +69,7 @@ const TransactionCard = ({
 };
 
 const Transactions = () => {
-  const { currentAccount } = useContext(TransactionContext);
+  const { transactions, currentAccount } = useContext(TransactionContext);
 
   return (
     <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
@@ -80,13 +80,13 @@ const Transactions = () => {
           </h3>
         ) : (
           <h3 className="text-white text-3xl text-center my-2">
-            Connect your account to see your transactions
+            Connect your account to see the latest transactions
           </h3>
         )}
 
         <div className="flex flex-wrap justify-center items-center mt-10">
-          {dummyData.reverse().map((transaction, index) => (
-            <TransactionCard key={index} {...transaction} />
+          {[...dummyData, ...transactions].reverse().map((transaction, i) => (
+            <TransactionCard key={i} {...transaction} />
           ))}
         </div>
       </div>
