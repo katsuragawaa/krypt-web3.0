0a75361412a34cd0043c18dfdb38389600921668
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-04 22:09:41 -0300

    Use custom hook

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 
 import { TransactionContext } from '../context/TransactionContext';
 
-// import useFetch from "../hooks/useFetch";
+import useFetch from "../hooks/useFetch";
 import dummyData from '../utils/dummyData';
 import { shortenAddress } from '../utils/shortenAddress';
 
@@ -15,7 +15,7 @@ const TransactionCard = ({
   amount,
   url,
 }) => {
-  // const gifUrl = useFetch({ keyword });
+  const gifUrl = useFetch({ keyword });
 
   return (
     <div
@@ -56,12 +56,12 @@ const TransactionCard = ({
           )}
         </div>
         <img
-          // src={gifUrl || url}
+          src={gifUrl || url}
           alt="nature"
           className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"
         />
         <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
-          <p className="text-[#37c7da] font-bold">{timestamp}</p>
+          <p className="text-white font-bold">{timestamp}</p>
         </div>
       </div>
     </div>
